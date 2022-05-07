cfdc15f3211706c704b7edf6644c60fa56470942
Simon Uvarov <2471506+simonuvarov@users.noreply.github.com>
2022-05-07 13:06:46 +0300

    remove unused functions

--- tabular/utils.py
+++ tabular/utils.py
@@ -1,31 +1,6 @@
 import numpy as np
 
 
-def to_grid_coordinates(state):
-    '''
-    Converts a state to a row, column matrix
-    '''
-    return state % 4, state // 4
-
-
-def to_linear_coordinates(row, col):
-    '''
-    Converts a row, column matrix to a state
-    '''
-    return row * 4 + col
-
-
-def print_as_grid(state_values):
-    '''
-    Prints array values as a grid
-    '''
-    for row in range(4):
-        for col in range(4):
-            state = to_linear_coordinates(row, col)
-            print("{:.4f}".format(state_values[state]), end=' ')
-        print()
-
-
 def random_argmax(a):
     '''
     Return the index of the maximum value in the array.
