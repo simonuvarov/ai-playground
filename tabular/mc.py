9c12d4d8f708c840fd97e5cd9a143b6f168a600f
Simon Uvarov <2471506+simonuvarov@users.noreply.github.com>
2022-05-23 12:13:30 +0300

    refactor MC by doing backward return calculation

--- tabular/mc.py
+++ tabular/mc.py
@@ -8,20 +8,10 @@ logging.basicConfig(level=logging.INFO)
 
 
 class MonteCarlo_Agent(Agent):
-    def calculate_return(self, trajectory):
-        '''
-        Calculate the return of the first state in a trajectory
-        trajectory: list of tuples (state, action, reward, next_state)
-        '''
-        r = 0
-        for index, transition in enumerate(trajectory):
-            reward = transition[2]
-            r += (self.gamma ** index) * reward
-        return r
-
     def update_Q_table(self, trajectory):
-        for index, (state, action, reward, next_state) in enumerate(trajectory):
-            G = self.calculate_return(trajectory[index:])
+        G = 0
+        for (state, action, reward, _) in trajectory[::-1]:
+            G = self.gamma * G + reward
             self.Q[state, action] += self.alpha * \
                 (G - self.Q[state, action])
 
